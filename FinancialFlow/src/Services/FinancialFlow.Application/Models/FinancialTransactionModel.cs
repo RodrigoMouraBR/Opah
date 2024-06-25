9997fc3530945ce25bb2a6ec28b21fea8721657b
Rodrigo Moura <rodrigomoura.mva@outlook.com>
2024-06-24 21:01:52 -0300

    Ajutando pequenos bugs

--- FinancialFlow/src/Services/FinancialFlow.Application/Models/FinancialTransactionModel.cs
+++ FinancialFlow/src/Services/FinancialFlow.Application/Models/FinancialTransactionModel.cs
@@ -6,7 +6,7 @@ namespace FinancialFlow.Application.Models
     public class FinancialTransactionModel
     {        
         public Guid CustomerId { get; set; }
-        public DateTime DateRef { get; private set; }
+        public DateTime DateRef { get; set; }
         public decimal Amount { get; set; }
         public string Description { get; set; } = string.Empty;
         public ETransactionType TransactionType { get; set; }
